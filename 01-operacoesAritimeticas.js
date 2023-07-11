8542e7536f9d4553c99d0e8153d220b2b02af4ef
Guilherme Mazutti <mazuttimail@gmail.com>
2023-07-11 20:50:52 -0300

    primeiro commit - operações aritiméticas

---
+++ 01-operacoesAritimeticas.js
@@ -0,0 +1,11 @@
+let num1 = 2;
+let num2 = 4;
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let divisao = num1 / num2;
+let multiplicacao = num1 * num2;
+
+console.log(`Soma: ${num1} + ${num2} = ${soma}`);
+console.log(`Subtração: ${num1} - ${num2} = ${subtracao}`);
+console.log(`Divisão: ${num1} / ${num2} = ${divisao}`);
+console.log(`Multiplicação: ${num1} * ${num2} = ${multiplicacao}`);
