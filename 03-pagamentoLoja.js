500091a8c082b0300e1843901d0dee0dd7735117
Guilherme Mazutti <mazuttimail@gmail.com>
2023-07-11 21:43:00 -0300

    exercicio 3

---
+++ 03-pagamentoLoja.js
@@ -0,0 +1,8 @@
+let preco = 60;
+let cash = preco - (preco * 10)/100;
+let parcela3 = preco / 3;
+
+
+console.log(`Preço: ${preco},00`);
+console.log(`À vista: ${cash},00`);
+console.log(`Ou 3x de: ${parcela3},00`);
