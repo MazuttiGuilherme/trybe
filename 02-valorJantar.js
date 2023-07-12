f7b75f4ee17737c124ede8c2fd83960c3ee6ff9a
Guilherme Mazutti <mazuttimail@gmail.com>
2023-07-11 21:28:38 -0300

    exercicio valor do jantar

---
+++ 02-valorJantar.js
@@ -0,0 +1,7 @@
+let valor = 80;
+let taxa = (valor * 10) / 100;
+let total = valor + taxa;
+
+console.log(`Valor do jantar R$${valor},00`);
+console.log(`Taxa do garçon: R$${taxa},00`);
+console.log(`Taxa do garçon: R$${total},00`);
